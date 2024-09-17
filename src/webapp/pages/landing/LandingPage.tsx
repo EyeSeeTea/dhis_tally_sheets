0e27b0ea2c5d069d12ebd06d97bff4458bad4f32
p3rcypj <pablo@percypj.com>
2024-09-17 08:19:13 +0000

    Second tear down LandingPage

--- src/webapp/pages/landing/LandingPage.tsx
+++ src/webapp/pages/landing/LandingPage.tsx
@@ -19,216 +19,71 @@ import {
     Tooltip,
     useTheme,
 } from "@material-ui/core";
-import { MultipleSelector } from "$/webapp/components/multiple-selector/MultipleSelector";
+import {
+    MultipleSelector,
+    MultipleSelectorProps,
+} from "$/webapp/components/multiple-selector/MultipleSelector";
 import { useAppContext } from "$/webapp/contexts/app-context";
-import { BasicDataSet } from "$/domain/entities/BasicDataSet";
 import { DataSetTable } from "$/webapp/components/dataset-table/DataSetTable";
-import { Id } from "$/domain/entities/Ref";
 import { OrgUnitSelector } from "$/webapp/components/org-unit-selector/OrgUnitSelector";
-import { OrgUnit } from "$/domain/entities/OrgUnit";
-import { useSnackbar } from "@eyeseetea/d2-ui-components";
-import { useBooleanState } from "$/webapp/utils/use-boolean";
 import { SettingsDialog } from "$/webapp/components/settings-dialog/SettingsDialog";
 import { DisableableTooltip } from "$/webapp/components/disableable-tooltip/DisableableTooltip";
-import { useLanguageSelector } from "$/webapp/pages/landing/useLanguageSelector";
-import { useDataSets } from "$/webapp/pages/landing/useDataSets";
-import { useDataSetSelector } from "$/webapp/pages/landing/useDataSetSelector";
+import { useLandingPage } from "$/webapp/pages/landing/useLandingPage";
+import { OrgUnit } from "$/domain/entities/OrgUnit";
 import i18n from "$/utils/i18n";
 import _c from "$/domain/entities/generic/Collection";
 import "./landing-page.css";
 
 export const LandingPage: React.FC = React.memo(() => {
-    const { config, compositionRoot, currentUser } = useAppContext();
-
     const theme = useTheme();
-    const classes = useStyles();
-    const snackbar = useSnackbar();
-
-    const [isSettingsOpen, { enable: openSettings, disable: closeSettings }] =
-        useBooleanState(false);
-    const [options, setOptions] = React.useState({
-        includeHeaders: true,
-    });
-
-    const handleChange = React.useCallback(
-        (event: React.ChangeEvent<HTMLInputElement>) => {
-            setOptions({ ...options, [event.target.name]: event.target.checked });
-        },
-        [options]
-    );
 
     const {
-        props: dataSetSelectorProps,
-        resetSelected: resetSelectedDataSets,
+        loading,
         orgUnits,
-        setOrgUnits,
-    } = useDataSetSelector();
-
-    const selectedDataSets = dataSetSelectorProps.selectedItems;
-
-    const availableLocales = React.useMemo(
-        () => getAvailableLocales(selectedDataSets),
-        [selectedDataSets]
-    );
-
-    const languageSelectorProps = useLanguageSelector(
-        availableLocales,
-        currentUser.preferredLocale,
-        currentUser.authorizations.canSelectAllLocales
-    );
-
-    const selectedLocales = languageSelectorProps.selectedItems;
-
-    const { dataSets, removeSection, loadingDataSets } = useDataSets(selectedDataSets);
-
-    const loading =
-        dataSetSelectorProps.loading === "loading" ||
-        languageSelectorProps.loading === "loading" ||
-        loadingDataSets;
-
-    const onRemoveSection = React.useMemo(
-        () => (dataSetId: Id) => (sectionId: Id) => {
-            removeSection(dataSetId, sectionId);
-        },
-        [removeSection]
-    );
-
-    const exportToExcel = React.useCallback(() => {
-        if (_c(dataSets).isNotEmpty() && _c(selectedLocales).isNotEmpty())
-            compositionRoot.dataSets.export
-                .execute({
-                    dataSets,
-                    locales: selectedLocales,
-                    config,
-                    includeHeaders: options.includeHeaders,
-                })
-                .run(
-                    () => {
-                        console.debug(`Exported to Excel ${dataSets.length} datasets`);
-                    },
-                    err => {
-                        snackbar.error(
-                            i18n.t("Something went wrong while creating the excel file")
-                        );
-                        console.error(err);
-                    }
-                );
-    }, [compositionRoot, config, dataSets, options.includeHeaders, selectedLocales, snackbar]);
-
-    const resetView = React.useCallback(() => {
-        resetSelectedDataSets();
-        setOptions({ includeHeaders: true });
-        setOrgUnits([]);
-    }, [resetSelectedDataSets, setOrgUnits]);
-
-    const setSelectedOrgUnits = React.useCallback(
-        (orgUnits: OrgUnit[]) => {
-            resetSelectedDataSets();
-            setOrgUnits(orgUnits);
-        },
-        [resetSelectedDataSets, setOrgUnits]
-    );
-
-    const disabledRestore =
-        loading ||
-        (_c(selectedDataSets).isEmpty() && _c(selectedLocales).isEmpty() && _c(orgUnits).isEmpty());
+        isSettingsOpen,
+        openSettings,
+        closeSettings,
+        handleChange,
+        onRemoveSection,
+        exportToExcel,
+        resetView,
+        setSelectedOrgUnits,
+        disabledRestore,
+        dataSets,
+        dataSetSelectorProps,
+        languageSelectorProps,
+        includeHeaders,
+        disabledExport,
+        allDataSetsSelected,
+    } = useLandingPage();
 
     return (
         <Box margin={theme.spacing(0.5)}>
-            {config.infoPlaceholder && (
-                <Box
-                    marginBottom={theme.spacing(0.25)}
-                    border={`1px solid ${theme.palette.primary.light}`}
-                    borderRadius={theme.shape.borderRadius}
-                >
-                    <Alert severity="info" variant="standard">
-                        {config.infoPlaceholder}
-                    </Alert>
-                </Box>
-            )}
+            <MessagePlaceholder />
             <Paper elevation={2}>
                 <Box padding={theme.spacing(3, 4, 2.5)}>
                     <Box display="flex" justifyContent="space-between" alignItems="center">
-                        <Box display="flex" gridColumnGap={theme.spacing(2)} alignItems="center">
-                            <OrgUnitSelector
-                                onChange={setSelectedOrgUnits}
-                                selected={orgUnits}
-                                disabled={loading}
-                            />
+                        <Selectors
+                            loading={loading}
+                            orgUnits={orgUnits}
+                            disabledRestore={disabledRestore}
+                            dataSetSelectorProps={dataSetSelectorProps}
+                            languageSelectorProps={languageSelectorProps}
+                            setSelectedOrgUnits={setSelectedOrgUnits}
+                            resetView={resetView}
+                        />
 
-                            <MultipleSelector {...dataSetSelectorProps} />
-                            <MultipleSelector {...languageSelectorProps} />
-
-                            <DisableableTooltip
-                                title={i18n.t("Restore changes")}
-                                disabled={disabledRestore}
-                            >
-                                <Button
-                                    className={classes.iconButton}
-                                    aria-label="restore"
-                                    size="small"
-                                    variant="outlined"
-                                    disabled={disabledRestore}
-                                    onClick={resetView}
-                                >
-                                    <RestoreIcon fontSize="medium" />
-                                </Button>
-                            </DisableableTooltip>
-
-                            {loading && (
-                                <Box display="flex" alignItems="center">
-                                    <CircularProgress size="2em" thickness={4} />
-                                </Box>
-                            )}
-                        </Box>
-
-                        <Box display="flex" gridColumnGap={theme.spacing(2)}>
-                            {currentUser.isAdmin() && (
-                                <Tooltip title={i18n.t("Settings")}>
-                                    <Button
-                                        className={classes.iconButton}
-                                        aria-label="settings"
-                                        size="small"
-                                        variant="outlined"
-                                        onClick={openSettings}
-                                    >
-                                        <SettingsIcon fontSize="medium" />
-                                    </Button>
-                                </Tooltip>
-                            )}
-
-                            <Button
-                                className={classes.actionButton}
-                                variant="outlined"
-                                color="default"
-                                startIcon={<PrintIcon />}
-                                onClick={window.print}
-                            >
-                                {i18n.t("Print")}
-                            </Button>
-
-                            <Button
-                                className={classes.actionButton}
-                                variant="contained"
-                                color="primary"
-                                startIcon={<DownloadIcon />}
-                                onClick={exportToExcel}
-                                disableElevation
-                                disabled={
-                                    _c(selectedDataSets).isEmpty() ||
-                                    _c(selectedLocales).isEmpty() ||
-                                    loading
-                                }
-                            >
-                                {i18n.t("Export to Excel")}
-                            </Button>
-                        </Box>
+                        <Actions
+                            openSettings={openSettings}
+                            exportToExcel={exportToExcel}
+                            disabledExport={disabledExport}
+                        />
                     </Box>
                     <Box marginTop={theme.spacing(0.25)}>
                         <FormControlLabel
                             control={
                                 <Checkbox
-                                    checked={options.includeHeaders}
+                                    checked={includeHeaders}
                                     onChange={handleChange}
                                     name="includeHeaders"
                                     color="primary"
@@ -240,7 +95,7 @@ export const LandingPage: React.FC = React.memo(() => {
                 </Box>
             </Paper>
 
-            {!dataSetSelectorProps.allSelected && _c(dataSets).isNotEmpty() && (
+            {!allDataSetsSelected && _c(dataSets).isNotEmpty() && (
                 <Box
                     className="print-zone"
                     marginTop={theme.spacing(0.5)}
@@ -250,7 +105,7 @@ export const LandingPage: React.FC = React.memo(() => {
                 >
                     {dataSets.map(ds => (
                         <DataSetTable
-                            includeHeaders={options.includeHeaders}
+                            includeHeaders={includeHeaders}
                             key={ds.id}
                             dataSet={ds}
                             onRemoveSection={onRemoveSection(ds.id)}
@@ -264,15 +119,134 @@ export const LandingPage: React.FC = React.memo(() => {
     );
 });
 
-function getAvailableLocales(dataSets: BasicDataSet[]): string[] {
-    const avail = _c(dataSets.map(ds => ds.getAvailableLocaleCodes()))
-        .flatten()
-        .concat("en") // Add English because is not included in translations
-        .uniq()
-        .compact()
-        .value();
-    return _c(dataSets).isEmpty() ? [] : avail;
+interface SelectorsProps {
+    loading: boolean;
+    orgUnits: OrgUnit[];
+    disabledRestore: boolean;
+    dataSetSelectorProps: MultipleSelectorProps;
+    languageSelectorProps: MultipleSelectorProps;
+    setSelectedOrgUnits: (orgUnits: OrgUnit[]) => void;
+    resetView: () => void;
 }
+
+const Selectors: React.FC<SelectorsProps> = React.memo(props => {
+    const {
+        loading,
+        orgUnits,
+        disabledRestore,
+        dataSetSelectorProps,
+        languageSelectorProps,
+        setSelectedOrgUnits,
+        resetView,
+    } = props;
+
+    const theme = useTheme();
+    const classes = useStyles();
+
+    return (
+        <Box display="flex" gridColumnGap={theme.spacing(2)} alignItems="center">
+            <OrgUnitSelector
+                onChange={setSelectedOrgUnits}
+                selected={orgUnits}
+                disabled={loading}
+            />
+
+            <MultipleSelector {...dataSetSelectorProps} />
+            <MultipleSelector {...languageSelectorProps} />
+
+            <DisableableTooltip title={i18n.t("Restore changes")} disabled={disabledRestore}>
+                <Button
+                    className={classes.iconButton}
+                    aria-label="restore"
+                    size="small"
+                    variant="outlined"
+                    disabled={disabledRestore}
+                    onClick={resetView}
+                >
+                    <RestoreIcon fontSize="medium" />
+                </Button>
+            </DisableableTooltip>
+
+            {loading && (
+                <Box display="flex" alignItems="center">
+                    <CircularProgress size="2em" thickness={4} />
+                </Box>
+            )}
+        </Box>
+    );
+});
+
+interface ActionsProps {
+    openSettings: () => void;
+    exportToExcel: () => void;
+    disabledExport: boolean;
+}
+
+const Actions: React.FC<ActionsProps> = React.memo(props => {
+    const { openSettings, exportToExcel, disabledExport } = props;
+    const { currentUser } = useAppContext();
+
+    const theme = useTheme();
+    const classes = useStyles();
+
+    return (
+        <Box display="flex" gridColumnGap={theme.spacing(2)}>
+            {currentUser.isAdmin() && (
+                <Tooltip title={i18n.t("Settings")}>
+                    <Button
+                        className={classes.iconButton}
+                        aria-label="settings"
+                        size="small"
+                        variant="outlined"
+                        onClick={openSettings}
+                    >
+                        <SettingsIcon fontSize="medium" />
+                    </Button>
+                </Tooltip>
+            )}
+
+            <Button
+                className={classes.actionButton}
+                variant="outlined"
+                color="default"
+                startIcon={<PrintIcon />}
+                onClick={window.print}
+            >
+                {i18n.t("Print")}
+            </Button>
+
+            <Button
+                className={classes.actionButton}
+                variant="contained"
+                color="primary"
+                startIcon={<DownloadIcon />}
+                onClick={exportToExcel}
+                disableElevation
+                disabled={disabledExport}
+            >
+                {i18n.t("Export to Excel")}
+            </Button>
+        </Box>
+    );
+});
+
+const MessagePlaceholder: React.FC = React.memo(() => {
+    const { config } = useAppContext();
+    const theme = useTheme();
+
+    if (!config.infoPlaceholder) return null;
+    return (
+        <Box
+            marginBottom={theme.spacing(0.25)}
+            border={`1px solid ${theme.palette.primary.light}`}
+            borderRadius={theme.shape.borderRadius}
+        >
+            <Alert severity="info" variant="standard">
+                {config.infoPlaceholder}
+            </Alert>
+        </Box>
+    );
+});
 
 const useStyles = makeStyles((_theme: Theme) =>
     createStyles({
